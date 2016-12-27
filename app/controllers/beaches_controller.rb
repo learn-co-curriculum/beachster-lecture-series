3dfa1f498bdbc445678b03b0c859a26f9cd22a5c
Luke Ghenco <lukeghenco@gmail.com>
2016-12-27 07:17:03 -0500

    added beaches#show that correctly displays a beach or an error message

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -4,4 +4,13 @@ class BeachesController < ApplicationController
     render json: Beach.all
   end
 
+  def show
+    beach = Beach.find_by_id(params[:id])
+    if beach
+      render json: beach
+    else
+      render json: { error: 'Sorry, this is not a beach' }
+    end
+  end
+
 end
