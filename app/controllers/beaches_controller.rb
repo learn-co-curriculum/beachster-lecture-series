1c63d112f04ba7f75480c761140208e1e8e8d40c
Luke Ghenco <lukeghenco@gmail.com>
2016-12-27 08:11:05 -0500

    added beaches#edit method

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,4 +1,5 @@
 class BeachesController < ApplicationController
+  before_action :set_beach
 
   def index
     render json: Beach.all
@@ -7,22 +8,34 @@ class BeachesController < ApplicationController
   def create
     beach = Beach.new(beach_params)
     if beach.save
-      render json: beach
+      render json: { beach: beach, message: "#{beach.title} was successfully created!" }
     else
-      render json: { error: 'There was an issue creating this beach' }
+      render json: { error: 'There was an issue creating this beach!' }
     end
   end
 
   def show
-    beach = Beach.find_by_id(params[:id])
-    if beach
-      render json: beach
+    if @beach
+      render json: @beach
     else
-      render json: { error: 'Sorry, this is not a beach' }
+      render json: { error: 'Sorry, this is not a beach!' }
     end
   end
 
+  def edit
+    if @beach.update(beach_params)
+      render json: { beach: @beach, message: "#{@beach.title} was successfully updated!" }
+    else
+      render json: { error: "#{@beach.title} was not updated correctly!"}
+    end
+  end
+
+
   private
+
+    def set_beach
+      @beach = Beach.find_by_id(params[:id])
+    end
 
     def beach_params
       params.require(:beach).permit(:title, :city, :state, :latitude, :longitude, :rating, :photo_url)
