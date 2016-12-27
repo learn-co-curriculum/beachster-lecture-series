ceca89aff880245a6be94834497dff1edf77184b
Luke Ghenco <lukeghenco@gmail.com>
2016-12-27 08:06:30 -0500

    finished beaches#create route which creates new beach

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -4,6 +4,15 @@ class BeachesController < ApplicationController
     render json: Beach.all
   end
 
+  def create
+    beach = Beach.new(beach_params)
+    if beach.save
+      render json: beach
+    else
+      render json: { error: 'There was an issue creating this beach' }
+    end
+  end
+
   def show
     beach = Beach.find_by_id(params[:id])
     if beach
@@ -12,5 +21,11 @@ class BeachesController < ApplicationController
       render json: { error: 'Sorry, this is not a beach' }
     end
   end
+
+  private
+
+    def beach_params
+      params.require(:beach).permit(:title, :city, :state, :latitude, :longitude, :rating, :photo_url)
+    end
 
 end
