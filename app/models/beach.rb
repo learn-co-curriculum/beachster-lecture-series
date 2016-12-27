88a27c92d0f38cf543cbea392ca89bbe9c5bc53e
Luke Ghenco <lukeghenco@gmail.com>
2016-12-27 07:12:34 -0500

    replace location validation with state validation

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,3 +1,3 @@
 class Beach < ApplicationRecord
-  validates :title, :city, :location, :latitude, :longitude, :photo_url, presence: true
+  validates :title, :city, :state, :latitude, :longitude, :photo_url, presence: true
 end
