ce989e42e62b927d83cd01b3809e9af512d9db62
Luke Ghenco <lukeghenco@gmail.com>
2017-01-10 17:49:16 -0500

    added authentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # omniauth authentication
+  post "/auth/:provider/callback", to: 'sessions#create'
+  delete "/signout", as: :signout, to: 'sessions#destroy'
+  get '/auth/failure', as: :failure, to: 'sessions#failure'
+  
 end
